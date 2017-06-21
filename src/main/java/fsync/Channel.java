253ee163dfc9bde7301b169b0542393ab42c7dac
Armin <me@obrown.io>
2017-06-21 08:56:24 +0200

    use correct channel

--- src/main/java/fsync/Channel.java
+++ src/main/java/fsync/Channel.java
@@ -3,29 +3,29 @@ package fsync;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
 
 public class Channel implements IO {
-  private FileChannel channel;
-  private RandomAccessFile raf;
+    private final FileChannel channel;
 
-  public Channel(File file) throws IOException {
-    raf = new RandomAccessFile(file, "rw");
-    channel = raf.getChannel();
-  }
+    public Channel(File file) throws IOException {
+        this.channel =
+            FileChannel.open(file.toPath(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    }
 
-  public void write(ByteBuffer buffer) throws IOException {
-    channel.write(buffer);
-  }
+    @Override
+    public void write(ByteBuffer buffer) throws IOException {
+        this.channel.write(buffer);
+    }
 
-  public void sync() throws IOException {
-    channel.force(false);
-  }
+    @Override
+    public void sync() throws IOException {
+        this.channel.force(false);
+    }
 
-  public void close() throws Exception {
-    raf.close();
-    channel.close();
-  }
+    @Override
+    public void close() throws Exception {
+        this.channel.close();
+    }
 }
